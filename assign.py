50c85363c8b0c580583c2b04e99681dad5a6d4df
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2018-01-17 14:46:07 +0800

    add no assign list

--- assign.py
+++ assign.py
@@ -13,23 +13,33 @@ from mysf import *
 sys.setdefaultencoding('utf8')
 from selenium.webdriver.common.by import By 
 
-case_id, assignee = data.split(' ')
+try:
+	case_id, assignee = data.split(' ')
+except:
+	case_id = data
 if case_id == 'all':
 	rp = get_assign_rules()
 	for report_id, rules in rp.iteritems():
 		caselist = sf.fetch_case_from_report(browser, report_id)
 		print "New Case:", len(caselist)
+		noassighlist= []
 		for case in caselist:
 			if case == {}:
 				continue
+			is_match = False
 			for rule in rules:
 				if not mysf.check_rule_permission(case, rule):
 					print "No permission for this case ", case
 					continue
 				if mysf.match_rule(case, rule['condition']):
 					alias, user_id = get_assignee(rule['queue_id'], rule['assignee'], rule['next'])
-					print "assign case %s:%s to %s:%s" % (case['Case Number'],case['Case ID'],  alias, user_id )
+					print "assign case %s:%s to %s:%s by condition:%s" % (case['Case Number'],case['Case ID'],  alias, user_id, rule['condition'] )
 					assign_case(browser, case['Case ID'], user_id)	
+					is_match = True
+			if not is_match:
+				noassighlist.append(case)
+		for case in noassighlist:
+			print "not match in %s case list:" % report_id, case
 else:
 	user_id = get_user_prop(assignee, 'sf_id')
 	if case_id.isdigit():
