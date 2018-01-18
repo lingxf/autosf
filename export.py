4ba77765c9d75046cfec6da5de3edeba41aeeabd
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2018-01-18 15:16:53 +0800

    add kba export

--- export.py
+++ export.py
@@ -35,6 +35,25 @@ elif cmd == 'export':
 		WebDriverWait(browser,300, 1).until(EC.presence_of_element_located((By.XPATH, title)))
 		print "Rule page open, click export rules..."
 		click_timeout(browser,  title)
+	elif data == 'kba':
+		url = "https://aggregator.qualcomm.com/Views/Pages/KnowledgeBases.aspx"
+		browser.get(url)
+		xpath = "//a[@class='k-button k-button-icontext k-grid-excel']"
+		WebDriverWait(browser,10, 1).until(EC.presence_of_element_located((By.XPATH, xpath)))
+		print "Find xpath:", xpath
+		is_quit = 20
+		while is_quit > 0:
+			sleep(1)
+			is_quit -= 1
+			rows = browser.find_elements_by_xpath(xpath)
+			if rows == []:
+				continue
+			print rows
+			for onerow in rows:
+				print onerow.get_attribute('innerHTML')
+				onerow.click()
+				is_quit = 0
+				break
 	else: #SF report
 		if data == 'today' or data == '':
 			report_id = '00O3A000009OpFh' #today
