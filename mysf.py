875bc373d1a746e22bcd353bfbb032e304f0ed0d
Xiaofeng(Daniel) Ling <xling@qti.qualcomm.com>
2018-01-31 16:45:50 +0800

    get_rcatask by owner

--- mysf.py
+++ mysf.py
@@ -218,8 +218,11 @@ def finish_rcatask(jobid, status):
 	c=db.cursor()
 	c.execute(sql)
 
-def get_rcatasks(status):
-	sql = "select * from rcaqueue where status = %d order by timestamp asc " % status
+def get_rcatasks(status, owner=''):
+	if owner == '':
+		sql = "select * from rcaqueue where status = %d order by timestamp asc " % status
+	else:
+		sql = "select * from rcaqueue where status = %d and owner = '%s' order by timestamp asc " % (status, owner)
 	return get_dicts_from_sql(sql)
 
 def get_rcatask(case):
